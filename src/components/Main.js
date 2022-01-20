a840905aaa0e387183475883377601e7afa4d5ba
Alwaz <alwazkazi3@gmail.com>
2022-01-20 14:18:41 -0800

    fixed typo again

--- src/components/Main.js
+++ src/components/Main.js
@@ -196,7 +196,7 @@ const Main = () => {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
           >
-            Blogs
+            Blog
           </motion.h2>
         </BLOG>
         <WORK to="/work" click={click}>
